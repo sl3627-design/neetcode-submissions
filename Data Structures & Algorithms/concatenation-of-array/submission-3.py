23e2e55041c155c3de800e079e90972c144a8598
sl3627-design <sl3627@cornell.edu>
2026-07-01 15:56:36 +0900

    Add: concatenation-of-array - submission-3

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        # Solution 1
+        # nums.extend(nums)
+        # return nums
+
+        # Solution 2
+        # for i in range(len(nums)):
+        #     nums.append(nums[i])
+        
+        # return nums 
+
+        # Solution 3
+        # return nums + nums
+        return nums * 2
