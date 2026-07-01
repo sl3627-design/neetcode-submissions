7c73da87ed4535328b329faa5f3029e2e656f0bf
sl3627-design <sl3627@cornell.edu>
2026-07-02 07:41:44 +0900

    Add: time-needed-to-buy-tickets - submission-0

---
+++ Data Structures & Algorithms/time-needed-to-buy-tickets/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        time = 0; i = 0
+        while tickets[k] != 0:
+            i = i%len(tickets)
+            if tickets[i] > 0:
+                tickets[i] -= 1
+                i += 1
+                time += 1
+            else:
+                i += 1
+        
+        return time
