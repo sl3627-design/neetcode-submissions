d253c27db9293a45ec1fab1c9612f4760b19abf7
sl3627-design <sl3627@cornell.edu>
2026-07-03 20:34:00 +0900

    Add: unique-email-addresses - submission-1

---
+++ Data Structures & Algorithms/unique-email-addresses/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        # d = set()
+        # for email in emails:
+        #     temp = email.split("@")
+        #     domain = temp[1]
+        #     if "+" in temp[0]:
+        #         temp = temp[0].split("+")
+        #     temp[0] = temp[0].replace(".", "")
+        #     local = temp[0]
+        #     d.add((local, domain))    
+        
+        # return len(d)
+
+        unique = set()
+
+        for e in emails:
+            local, domain = e.split("@")
+            local = local.split("+")[0]
+            local = local.replace(".", "")
+            unique.add((local, domain))
+        
+        return len(unique)
+
+        
