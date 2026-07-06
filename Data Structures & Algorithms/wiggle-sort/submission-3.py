d127274dc150ab1ae39e23b086f3794133a13225
sl3627-design <sl3627@cornell.edu>
2026-07-06 22:34:41 +0900

    Add: wiggle-sort - submission-3

---
+++ Data Structures & Algorithms/wiggle-sort/submission-3.py
@@ -0,0 +1,9 @@
+class Solution:
+    def wiggleSort(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+  
+        for i in range(1, len(nums)):
+            if (i & 1 and nums[i] < nums[i-1]) or (not i & 1 and nums[i] > nums[i-1]):
+                nums[i], nums[i-1] = nums[i-1], nums[i]
