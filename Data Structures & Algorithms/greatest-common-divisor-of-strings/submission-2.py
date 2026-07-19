f2397491640d37cee3a320527ca7a7f0a63ac742
sl3627-design <sl3627@cornell.edu>
2026-07-19 10:45:08 +0900

    Add: greatest-common-divisor-of-strings - submission-2

---
+++ Data Structures & Algorithms/greatest-common-divisor-of-strings/submission-2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+
+        len_gcd = math.gcd(len(str1), len(str2))
+
+        if str2[:len_gcd]*(len(str1)//len_gcd) == str1:
+            return str1[:len_gcd]
+        
+        return ""
+
